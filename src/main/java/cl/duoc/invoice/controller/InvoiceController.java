b464897a583c22f01c9b8f2c3da6721b07639f2f
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-21 22:41:27 -0400

    feat: add delete endpoint method to controller class

--- src/main/java/cl/duoc/invoice/controller/InvoiceController.java
+++ src/main/java/cl/duoc/invoice/controller/InvoiceController.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,12 @@ public class InvoiceController {
     @GetMapping("/{folio}")
     public ResponseEntity<InvoiceResponseDto> getInvoiceByFolio(@PathVariable Long folio) {
         InvoiceResponseDto response = invoiceService.getInvoiceByFolio(folio);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{folio}/anular")
+    public ResponseEntity<InvoiceResponseDto> anularInvoice(@PathVariable Long folio) {
+        InvoiceResponseDto response = invoiceService.deleteInvoice(folio);
         return ResponseEntity.ok(response);
     }
 }
