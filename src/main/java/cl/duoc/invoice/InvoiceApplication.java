d2af61f14e553ee7d6c5921f1246afc9a40ede8a
lironscallealta <lironscallealta@gmail.com>
2026-06-25 20:04:00 -0400

    chore(deps): add Eureka, HATEOAS, datafaker and test dependencies

--- src/main/java/cl/duoc/invoice/InvoiceApplication.java
+++ src/main/java/cl/duoc/invoice/InvoiceApplication.java
@@ -8,8 +8,10 @@ package cl.duoc.invoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InvoiceApplication {
 
     public static void main(String[] args) {
--- src/test/java/cl/duoc/invoice/InvoiceApplicationTests.java
+++ src/test/java/cl/duoc/invoice/InvoiceApplicationTests.java
@@ -8,8 +8,10 @@ package cl.duoc.invoice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InvoiceApplicationTests {
 
     @Test
