01f15d21b81050193d519cdd14b6db3af366a971
polirritmico <ebray187@gmail.com>
2026-06-27 17:57:35 -0400

    fix(security): disable default user details service autoconfiguration

--- src/main/java/cl/duoc/invoice/InvoiceApplication.java
+++ src/main/java/cl/duoc/invoice/InvoiceApplication.java
@@ -8,9 +8,10 @@ package cl.duoc.invoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class InvoiceApplication {
 
